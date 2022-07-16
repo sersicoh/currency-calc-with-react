c10a899b652c280d296007b8602e7d7a2ac555b1
Wojciech Szymczak <sersicoh@gmail.com>
2022-07-16 15:07:51 +0200

    added styled-components

--- src/App/Header/styled.js
+++ src/App/Header/styled.js
@@ -1,9 +1,8 @@
-.header {
+import styled from "styled-components";
+
+export const StyledHeader = styled.h1`
     padding: 5px 40px;
     text-shadow: 1px 0px 16px rgb(20 16 16);
-}
-
-.header__text {
     text-align: center;
     text-transform: uppercase;
-}
+`;
